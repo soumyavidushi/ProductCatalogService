62164590549f9fe43050e8184d373d5cd28c28f5
Soumya <soumya.vidushi@gmail.com>
2025-04-26 16:14:05 +0530

    Fetch Mode and Batch Size

--- src/main/java/org/example/productcatalogservice/models/Category.java
+++ src/main/java/org/example/productcatalogservice/models/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -17,6 +18,7 @@ public class Category extends BaseModel{
     String name;
     String description;
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
-    @Fetch(FetchMode.JOIN)
+    @Fetch(FetchMode.SELECT)
+    @BatchSize(size = 20)
     List<Product> products;
 }
